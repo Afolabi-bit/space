1173d5f55e34d11186ecee06550e0e973571a9fd
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-05-16 17:59:27 +0100

    check

---
+++ src/layout/FooterData.jsx
@@ -0,0 +1,26 @@
+import DetailsCard from "../utilities/DetailsCard";
+
+const FooterData = ({ details }) => {
+	return (
+		<div className="w-full flex flex-col gap-[8px]">
+			<DetailsCard
+				parameter={"rotation time"}
+				detail={details.rotation}
+			/>
+			<DetailsCard
+				parameter={"revolution time"}
+				detail={details.revolution}
+			/>
+			<DetailsCard
+				parameter={"radius"}
+				detail={details.radius}
+			/>
+			<DetailsCard
+				parameter={"average temp."}
+				detail={details.temperature}
+			/>
+		</div>
+	);
+};
+
+export default FooterData;
